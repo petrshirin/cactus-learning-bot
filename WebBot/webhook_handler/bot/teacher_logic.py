f9f54bd90780c6766c9f5890cb0771c8efb5c2d3
Petr <p.e.shirin@gmail.com>
2020-06-03 05:17:40 +0800

    add phrases

--- WebBot/webhook_handler/bot/teacher_logic.py
+++ WebBot/webhook_handler/bot/teacher_logic.py
@@ -495,7 +495,7 @@ class TeacherAction:
         message_text = ru.get('check_home_work_per_part')
         for user_part in user_parts:
             for user_task in user_part.tasks.filter(~Q(text_answer=None)| ~Q(answer_file=None)).all():
-                markup.add(types.InlineKeyboardButton(f'{user_task.name} {user_task.user.FIO}', callback_data=f'useranswer_{course_id}_{part_id}_{user_task.pk}'))
+                markup.add(types.InlineKeyboardButton(f'{user_task.task.name} {user_task.user.FIO}', callback_data=f'useranswer_{course_id}_{part_id}_{user_task.pk}'))
             tasks = user_parts.tasks.all()
         markup.add(types.InlineKeyboardButton('Назад', callback_data=f'coursepart_{course_id}_{part_id}'))
 
