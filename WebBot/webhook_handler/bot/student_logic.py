11647019188ded9838f7eb57e22508a722623a76
Petr <p.e.shirin@gmail.com>
2020-06-03 05:13:40 +0800

    add phrases

--- WebBot/webhook_handler/bot/student_logic.py
+++ WebBot/webhook_handler/bot/student_logic.py
@@ -136,7 +136,7 @@ class StudentAction:
         message_text = ru.get('student_task_files')
         user_task = UserTask.objects.filter(pk=task_id).first()
         for file in user_task.task.files.all():
-            markup.add(types.InlineKeyboardButton(file.filename, callback_data=f'downloadfile_{file.pk}'))
+            markup.add(types.InlineKeyboardButton(file.file.filename, callback_data=f'downloadfile_{file.pk}'))
         markup.add(types.InlineKeyboardButton('Назад', callback_data=f'task_{task_id}'))
         self.bot.edit_message_text(chat_id=self.message.chat.id, text=message_text,
                                    reply_markup=markup, message_id=self.message.message_id)
